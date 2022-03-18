c0625fcf8c0fe26886a7d64daa440567521afc62
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-03-18 17:52:03 +0500

    Initial Commit

---
+++ SwiftCalculator/SwiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalculatorApp.swift
+//  SwiftCalculator
+//
+//  Created by Thor on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
